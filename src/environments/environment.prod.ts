1c1ad0ec118efe9fdd9398bd144b69e46e5edfcb
Worawut <tonmanna@gmail.com>
2018-07-01 06:16:41 +0700

    Change Environment Path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  path : 'ttps://us-central1-labprojectapi-403f5.cloudfunctions.net/api_function'
+  path : 'https://us-central1-labprojectapi-403f5.cloudfunctions.net/api_function'
 };
